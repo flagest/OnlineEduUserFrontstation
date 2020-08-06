09c3893fd949d36ba74eebcfb51ee65758f09d91
zhengwujia <418894252@qq.com>
2020-08-07 06:28:59 +0800

    增加前端登录页面

---
+++ layouts/sign.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="sign">
+    <!--标题-->
+    <div class="logo">
+      <img src="~/assets/img/logo.png" alt="logo"/>
+    </div>
+    <!--表单-->
+  </div>
+</template>
+<script>
+  export default {}
+
+</script>
+<style>
+
+</style>
